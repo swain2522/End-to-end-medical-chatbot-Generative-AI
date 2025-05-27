1219057c741223479a6253f7e7d61fd2b222f2f2
Pradyumna Swain <swain2522@gmail.com>
2025-05-27 15:50:57 +0530

    Trail completed

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Swain2522',
+    author_email= 'swain2522@gmail.com',
     packages= find_packages(),
     install_requires = []
 
